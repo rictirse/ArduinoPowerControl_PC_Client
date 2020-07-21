b15b9cc4d02045fb94c5b58a949037d52facf851
DangHome <shoop0830@gmail.com>
2020-07-21 21:15:05 +0800

    Git_Init

---
+++ RS232/Form1.Designer.cs
@@ -0,0 +1,296 @@
+﻿namespace RS232
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.btn_Speaker = new System.Windows.Forms.Button();
+            this.Status = new System.Windows.Forms.Label();
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.PowerSwitch = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.sowWindowToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.tsm_Speaker = new System.Windows.Forms.ToolStripMenuItem();
+            this.tsm_Subwoofer = new System.Windows.Forms.ToolStripMenuItem();
+            this.tsm_Preamplifier = new System.Windows.Forms.ToolStripMenuItem();
+            this.tsm_Mixer = new System.Windows.Forms.ToolStripMenuItem();
+            this.tsm_Fan = new System.Windows.Forms.ToolStripMenuItem();
+            this.tsm_Extra = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.btn_Subwoofer = new System.Windows.Forms.Button();
+            this.btn_Mixer = new System.Windows.Forms.Button();
+            this.btn_Preamplifier = new System.Windows.Forms.Button();
+            this.btn_Fan = new System.Windows.Forms.Button();
+            this.btn_Extra = new System.Windows.Forms.Button();
+            this.btn_Resend = new System.Windows.Forms.Button();
+            this.PowerSwitch.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btn_Speaker
+            // 
+            this.btn_Speaker.Enabled = false;
+            this.btn_Speaker.Font = new System.Drawing.Font("Arial Narrow", 14F, System.Drawing.FontStyle.Bold);
+            this.btn_Speaker.Location = new System.Drawing.Point(16, 40);
+            this.btn_Speaker.Margin = new System.Windows.Forms.Padding(7, 6, 7, 6);
+            this.btn_Speaker.Name = "btn_Speaker";
+            this.btn_Speaker.Size = new System.Drawing.Size(105, 62);
+            this.btn_Speaker.TabIndex = 0;
+            this.btn_Speaker.Tag = "Speaker";
+            this.btn_Speaker.Text = "Speaker";
+            this.btn_Speaker.UseVisualStyleBackColor = true;
+            this.btn_Speaker.Click += new System.EventHandler(this.btn_Click);
+            // 
+            // Status
+            // 
+            this.Status.AutoSize = true;
+            this.Status.Font = new System.Drawing.Font("Arial Narrow", 14F, System.Drawing.FontStyle.Bold);
+            this.Status.Location = new System.Drawing.Point(19, 9);
+            this.Status.Name = "Status";
+            this.Status.Size = new System.Drawing.Size(123, 23);
+            this.Status.TabIndex = 1;
+            this.Status.Text = "RS232 Status : ";
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.ContextMenuStrip = this.PowerSwitch;
+            this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
+            this.notifyIcon1.Text = "PowerSwitch";
+            this.notifyIcon1.Visible = true;
+            this.notifyIcon1.DoubleClick += new System.EventHandler(this.notifyIcon1_DoubleClick);
+            // 
+            // PowerSwitch
+            // 
+            this.PowerSwitch.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.sowWindowToolStripMenuItem,
+            this.tsm_Speaker,
+            this.tsm_Subwoofer,
+            this.tsm_Preamplifier,
+            this.tsm_Mixer,
+            this.tsm_Fan,
+            this.tsm_Extra,
+            this.exitToolStripMenuItem});
+            this.PowerSwitch.Name = "PowerSwitch";
+            this.PowerSwitch.Size = new System.Drawing.Size(156, 180);
+            // 
+            // sowWindowToolStripMenuItem
+            // 
+            this.sowWindowToolStripMenuItem.Name = "sowWindowToolStripMenuItem";
+            this.sowWindowToolStripMenuItem.Size = new System.Drawing.Size(155, 22);
+            this.sowWindowToolStripMenuItem.Text = "Show Window";
+            this.sowWindowToolStripMenuItem.Click += new System.EventHandler(this.sowWindowToolStripMenuItem_Click);
+            // 
+            // tsm_Speaker
+            // 
+            this.tsm_Speaker.Enabled = false;
+            this.tsm_Speaker.Name = "tsm_Speaker";
+            this.tsm_Speaker.Size = new System.Drawing.Size(155, 22);
+            this.tsm_Speaker.Tag = "Speaker";
+            this.tsm_Speaker.Text = "Speaker";
+            this.tsm_Speaker.Click += new System.EventHandler(this.btn_Click);
+            // 
+            // tsm_Subwoofer
+            // 
+            this.tsm_Subwoofer.Enabled = false;
+            this.tsm_Subwoofer.Name = "tsm_Subwoofer";
+            this.tsm_Subwoofer.Size = new System.Drawing.Size(155, 22);
+            this.tsm_Subwoofer.Tag = "Subwoofer";
+            this.tsm_Subwoofer.Text = "Subwoofer";
+            this.tsm_Subwoofer.Click += new System.EventHandler(this.btn_Click);
+            // 
+            // tsm_Preamplifier
+            // 
+            this.tsm_Preamplifier.Enabled = false;
+            this.tsm_Preamplifier.Name = "tsm_Preamplifier";
+            this.tsm_Preamplifier.Size = new System.Drawing.Size(155, 22);
+            this.tsm_Preamplifier.Tag = "Preamplifier";
+            this.tsm_Preamplifier.Text = "Preamplifier";
+            this.tsm_Preamplifier.Click += new System.EventHandler(this.btn_Click);
+            // 
+            // tsm_Mixer
+            // 
+            this.tsm_Mixer.Enabled = false;
+            this.tsm_Mixer.Name = "tsm_Mixer";
+            this.tsm_Mixer.Size = new System.Drawing.Size(155, 22);
+            this.tsm_Mixer.Tag = "Mixer";
+            this.tsm_Mixer.Text = "Mixer";
+            this.tsm_Mixer.Click += new System.EventHandler(this.btn_Click);
+            // 
+            // tsm_Fan
+            // 
+            this.tsm_Fan.Enabled = false;
+            this.tsm_Fan.Name = "tsm_Fan";
+            this.tsm_Fan.Size = new System.Drawing.Size(155, 22);
+            this.tsm_Fan.Text = "Fan";
+            // 
+            // tsm_Extra
+            // 
+            this.tsm_Extra.Enabled = false;
+            this.tsm_Extra.Name = "tsm_Extra";
+            this.tsm_Extra.Size = new System.Drawing.Size(155, 22);
+            this.tsm_Extra.Text = "Extra";
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(155, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // btn_Subwoofer
+            // 
+            this.btn_Subwoofer.Enabled = false;
+            this.btn_Subwoofer.Font = new System.Drawing.Font("Arial Narrow", 14F, System.Drawing.FontStyle.Bold);
+            this.btn_Subwoofer.Location = new System.Drawing.Point(135, 40);
+            this.btn_Subwoofer.Margin = new System.Windows.Forms.Padding(7, 6, 7, 6);
+            this.btn_Subwoofer.Name = "btn_Subwoofer";
+            this.btn_Subwoofer.Size = new System.Drawing.Size(105, 62);
+            this.btn_Subwoofer.TabIndex = 0;
+            this.btn_Subwoofer.Tag = "Subwoofer";
+            this.btn_Subwoofer.Text = "Subwoofer";
+            this.btn_Subwoofer.UseVisualStyleBackColor = true;
+            this.btn_Subwoofer.Click += new System.EventHandler(this.btn_Click);
+            // 
+            // btn_Mixer
+            // 
+            this.btn_Mixer.Enabled = false;
+            this.btn_Mixer.Font = new System.Drawing.Font("Arial Narrow", 14F, System.Drawing.FontStyle.Bold);
+            this.btn_Mixer.Location = new System.Drawing.Point(135, 124);
+            this.btn_Mixer.Margin = new System.Windows.Forms.Padding(7, 6, 7, 6);
+            this.btn_Mixer.Name = "btn_Mixer";
+            this.btn_Mixer.Size = new System.Drawing.Size(105, 62);
+            this.btn_Mixer.TabIndex = 0;
+            this.btn_Mixer.Tag = "Mixer";
+            this.btn_Mixer.Text = "Mixer";
+            this.btn_Mixer.UseVisualStyleBackColor = true;
+            this.btn_Mixer.Click += new System.EventHandler(this.btn_Click);
+            // 
+            // btn_Preamplifier
+            // 
+            this.btn_Preamplifier.Enabled = false;
+            this.btn_Preamplifier.Font = new System.Drawing.Font("Arial Narrow", 13F, System.Drawing.FontStyle.Bold);
+            this.btn_Preamplifier.Location = new System.Drawing.Point(14, 124);
+            this.btn_Preamplifier.Margin = new System.Windows.Forms.Padding(7, 6, 7, 6);
+            this.btn_Preamplifier.Name = "btn_Preamplifier";
+            this.btn_Preamplifier.Size = new System.Drawing.Size(105, 62);
+            this.btn_Preamplifier.TabIndex = 0;
+            this.btn_Preamplifier.Tag = "Preamplifier";
+            this.btn_Preamplifier.Text = "Preamplifier";
+            this.btn_Preamplifier.UseVisualStyleBackColor = true;
+            this.btn_Preamplifier.Click += new System.EventHandler(this.btn_Click);
+            // 
+            // btn_Fan
+            // 
+            this.btn_Fan.Enabled = false;
+            this.btn_Fan.Font = new System.Drawing.Font("Arial Narrow", 14F, System.Drawing.FontStyle.Bold);
+            this.btn_Fan.Location = new System.Drawing.Point(254, 40);
+            this.btn_Fan.Margin = new System.Windows.Forms.Padding(7, 6, 7, 6);
+            this.btn_Fan.Name = "btn_Fan";
+            this.btn_Fan.Size = new System.Drawing.Size(105, 62);
+            this.btn_Fan.TabIndex = 0;
+            this.btn_Fan.Tag = "Fan";
+            this.btn_Fan.Text = "Fan";
+            this.btn_Fan.UseVisualStyleBackColor = true;
+            this.btn_Fan.Click += new System.EventHandler(this.btn_Click);
+            // 
+            // btn_Extra
+            // 
+            this.btn_Extra.Enabled = false;
+            this.btn_Extra.Font = new System.Drawing.Font("Arial Narrow", 14F, System.Drawing.FontStyle.Bold);
+            this.btn_Extra.Location = new System.Drawing.Point(254, 124);
+            this.btn_Extra.Margin = new System.Windows.Forms.Padding(7, 6, 7, 6);
+            this.btn_Extra.Name = "btn_Extra";
+            this.btn_Extra.Size = new System.Drawing.Size(105, 62);
+            this.btn_Extra.TabIndex = 0;
+            this.btn_Extra.Tag = "Extra";
+            this.btn_Extra.Text = "Extra";
+            this.btn_Extra.UseVisualStyleBackColor = true;
+            this.btn_Extra.Click += new System.EventHandler(this.btn_Click);
+            // 
+            // btn_Resend
+            // 
+            this.btn_Resend.Enabled = false;
+            this.btn_Resend.Location = new System.Drawing.Point(254, 9);
+            this.btn_Resend.Margin = new System.Windows.Forms.Padding(7, 6, 7, 6);
+            this.btn_Resend.Name = "btn_Resend";
+            this.btn_Resend.Size = new System.Drawing.Size(105, 22);
+            this.btn_Resend.TabIndex = 0;
+            this.btn_Resend.Tag = "Resend";
+            this.btn_Resend.Text = "Resend";
+            this.btn_Resend.UseVisualStyleBackColor = true;
+            this.btn_Resend.Click += new System.EventHandler(this.btn_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.ClientSize = new System.Drawing.Size(374, 201);
+            this.Controls.Add(this.Status);
+            this.Controls.Add(this.btn_Extra);
+            this.Controls.Add(this.btn_Mixer);
+            this.Controls.Add(this.btn_Fan);
+            this.Controls.Add(this.btn_Subwoofer);
+            this.Controls.Add(this.btn_Preamplifier);
+            this.Controls.Add(this.btn_Resend);
+            this.Controls.Add(this.btn_Speaker);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Margin = new System.Windows.Forms.Padding(7, 6, 7, 6);
+            this.MaximumSize = new System.Drawing.Size(390, 240);
+            this.MinimumSize = new System.Drawing.Size(390, 240);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "RS232 Power Switch";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Resize += new System.EventHandler(this.Form1_Resize);
+            this.PowerSwitch.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_Speaker;
+        private System.Windows.Forms.Label Status;
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+        private System.Windows.Forms.ContextMenuStrip PowerSwitch;
+        private System.Windows.Forms.ToolStripMenuItem tsm_Speaker;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem sowWindowToolStripMenuItem;
+        private System.Windows.Forms.Button btn_Subwoofer;
+        private System.Windows.Forms.Button btn_Mixer;
+        private System.Windows.Forms.Button btn_Preamplifier;
+        private System.Windows.Forms.ToolStripMenuItem tsm_Mixer;
+        private System.Windows.Forms.ToolStripMenuItem tsm_Subwoofer;
+        private System.Windows.Forms.ToolStripMenuItem tsm_Preamplifier;
+        private System.Windows.Forms.Button btn_Fan;
+        private System.Windows.Forms.Button btn_Extra;
+        private System.Windows.Forms.ToolStripMenuItem tsm_Extra;
+        private System.Windows.Forms.ToolStripMenuItem tsm_Fan;
+        private System.Windows.Forms.Button btn_Resend;
+    }
+}
+
---
+++ RS232/Form1.cs
@@ -0,0 +1,302 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+using System.IO.Ports;
+using System.Threading;
+using System.Diagnostics;
+
+namespace RS232
+{
+    public partial class Form1 : Form
+    {
+        public SerialPort serialPort = new SerialPort();
+        bool rs232status = false;
+        bool WaitCmd     = false;
+        bool FormHide    = false;
+        PowerStatus SW   = new PowerStatus();
+
+        [DllImport("user32.dll")]
+        private static extern bool SetForegroundWindow(IntPtr hWnd);
+
+        public Form1()
+        {
+            InitializeComponent();
+            CheckOpen();
+        }
+
+        private void CheckOpen()
+        {
+            string RunningProcess = Process.GetCurrentProcess().ProcessName;
+            Process[] processes = Process.GetProcessesByName(RunningProcess);
+            if (processes.Length == 2)
+            {
+                if (processes[0].StartTime > processes[1].StartTime)
+                {
+                    processes[1].Kill();
+                }
+                else
+                {
+                    processes[0].Kill();
+                }
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            if (Rs232PortDoor())
+            {
+                CtrlStatus(true);
+                UpdateUI();
+                Status.Text = "RS232 Status : Ready";
+            }
+        }
+
+        private bool Rs232PortDoor()
+        {
+            rs232status = true;
+            WaitCmd = true;
+            serialPort.PortName = "COM5";
+
+            // 檢查 PORT 是否關閉
+            if (!serialPort.IsOpen)
+                serialPort.Close();
+
+            serialPort.BaudRate = 9600;            // baud rate = 9600
+            serialPort.Parity   = Parity.None;     // Parity = none
+            serialPort.StopBits = StopBits.One;    // stop bits = one
+            serialPort.DataBits = 8;               // data bits = 8
+
+            serialPort.DataReceived += new SerialDataReceivedEventHandler(SerialPort_DataReceived);
+
+            try
+            {
+                serialPort.Open();
+            }
+            catch (InvalidOperationException e)
+            {
+                rs232status = false;
+            }
+
+            if (rs232status)
+            {
+                serialPort.DiscardInBuffer();                // RX
+                serialPort.DiscardOutBuffer();               // TX
+                rs232status = checkArduinoStatus("Check_status\r\n");
+            }
+
+            return rs232status;
+        }
+
+        private void rs232Output(string sCmd)
+        {
+            char[] cCmd = new char[16];
+            cCmd = sCmd.ToCharArray(0, sCmd.Length);
+
+            for (int i = 0; i < cCmd.Length; i++)
+            {
+                serialPort.Write(cCmd, i, 1);
+            }
+        }
+
+        void SerialPort_DataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+            char[] tmpCmd = new char[16];
+            string sCmd = "";
+            Thread.Sleep(100);
+            serialPort.Read(tmpCmd, 0, tmpCmd.Length);
+            for (int i = 0; i <= tmpCmd.Length; i++)
+            {
+                if (tmpCmd[i] == '\0' || tmpCmd[i] == '\r')
+                    break;
+                else
+                    sCmd = sCmd + tmpCmd[i].ToString();
+            }
+            CheckStatus(sCmd);
+        }
+
+        private bool checkArduinoStatus(string sCmd)
+        {
+            int iTimeCnt = 0;
+            rs232Output(sCmd);
+            while (WaitCmd)
+            {
+                Thread.Sleep(250);
+                iTimeCnt++;
+                if (iTimeCnt > 5)
+                {
+                    Status.Text = "RS232 Status : Fail";
+                    CtrlStatus(false);
+                    WaitCmd = false;
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        private void CtrlStatus(bool Status)
+        {
+            btn_Speaker.Enabled      = Status;
+            tsm_Speaker.Enabled      = Status;
+                                     
+            btn_Subwoofer.Enabled    = Status;
+            tsm_Subwoofer.Enabled    = Status;
+
+            btn_Preamplifier.Enabled = Status;
+            tsm_Preamplifier.Enabled = Status;
+
+            btn_Mixer.Enabled        = Status;
+            tsm_Mixer.Enabled        = Status;
+
+            btn_Fan.Enabled          = Status;
+            tsm_Fan.Enabled          = Status;
+
+            btn_Extra.Enabled        = Status;
+            tsm_Extra.Enabled        = Status;
+
+            btn_Resend.Enabled       = Status;
+        }
+
+        private void btn_Click(object sender, EventArgs e)
+        {
+            string tag;
+            tag = sender.GetType().Name == "Button" ? 
+                ((Control)sender).Tag.ToString() : 
+                ((ToolStripMenuItem)sender).Tag.ToString();
+            
+            CPowerSwitch(tag);
+        }
+
+        private void UpdateUI()
+        {
+            tsm_Speaker.Text      = btn_Speaker.Text      = SW.Speaker      == 1 ? "Speaker ON"      : "Speaker Off";
+            tsm_Subwoofer.Text    = btn_Subwoofer.Text    = SW.Subwoofer    == 1 ? "Subwoofer ON"    : "Subwoofer Off";
+            tsm_Preamplifier.Text = btn_Preamplifier.Text = SW.Preamplifier == 1 ? "Preamplifier ON" : "Preamplifier Off";
+            tsm_Mixer.Text        = btn_Mixer.Text        = SW.Mixer        == 1 ? "Mixer ON"        : "Mixer Off";
+            tsm_Fan.Text          = btn_Fan.Text          = SW.Fan          == 1 ? "Fan ON"          : "Fan Off";
+            tsm_Extra.Text        = btn_Extra.Text        = SW.Extra        == 1 ? "Extra ON"        : "Extra Off";
+        }
+
+        private void CPowerSwitch(string tag)
+        {
+            WaitCmd = true;
+            switch (tag)
+            {
+                case "Speaker":
+                    SW.Speaker = SW.Speaker == 0 ? 1 : 0;
+                    tsm_Speaker.Text = btn_Speaker.Text =
+                        SW.Speaker == 1 ? "Speaker ON" : "Speaker Off";
+                    break;
+                case "Subwoofer":
+                    SW.Subwoofer = SW.Subwoofer == 0 ? 1 : 0;
+                    tsm_Subwoofer.Text = btn_Subwoofer.Text = 
+                        SW.Subwoofer == 1 ? "Subwoofer ON" : "Subwoofer Off";
+                    break;
+                case "Preamplifier":
+                    SW.Preamplifier = SW.Preamplifier == 0 ? 1 : 0;
+                    tsm_Preamplifier.Text = btn_Preamplifier.Text = 
+                        SW.Preamplifier == 1 ? "Preamplifier ON" : "Preamplifier Off";
+                    break;
+                case "Mixer":
+                    SW.Mixer = SW.Mixer == 0 ? 1 : 0;
+                    tsm_Mixer.Text = btn_Mixer.Text = 
+                        SW.Mixer == 1 ? "Mixer ON" : "Mixer Off";
+                    break;
+                case "Fan":
+                    SW.Fan = SW.Fan == 0 ? 1 : 0;
+                    tsm_Fan.Text = btn_Fan.Text =
+                        SW.Fan == 1 ? "Fan ON" : "Fan Off";
+                    break;
+                case "Extra":
+                    SW.Extra = SW.Extra == 0 ? 1 : 0;
+                    tsm_Extra.Text = btn_Extra.Text =
+                        SW.Extra == 1 ? "Extra ON" : "Extra Off";
+                    break;
+                default:
+                    break;
+            }
+            rs232Output(SW.Output);
+        }
+
+        private void CheckStatus(string text)
+        {
+            WaitCmd = false;
+            string[] s = text.Split(',');
+
+            SW.Speaker      = int.Parse(s[0]);
+            SW.Subwoofer    = int.Parse(s[1]);
+            SW.Preamplifier = int.Parse(s[2]);
+            SW.Mixer        = int.Parse(s[3]);
+            SW.Fan          = int.Parse(s[4]);
+            SW.Extra        = int.Parse(s[5]);
+        }
+
+        private void Form1_Resize(object sender, EventArgs e)
+        {
+            if (WindowState == FormWindowState.Minimized)
+            {
+                Hide();
+                FormHide = true;
+            }
+        }
+
+        private void Form1_Hide()
+        {
+            if (WindowState == FormWindowState.Normal && FormHide == false)
+            {
+                FormHide = true;
+                Hide();
+            }
+            else
+            {
+                FormHide = false;
+                Show();
+                WindowState = FormWindowState.Normal;
+                SetForegroundWindow(Handle);
+            }
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void sowWindowToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Form1_Hide();
+        }
+
+        private void notifyIcon1_DoubleClick(object sender, EventArgs e)
+        {
+            Form1_Hide();
+        }
+
+        class PowerStatus
+        {
+            public int Speaker;
+            public int Subwoofer;
+            public int Preamplifier;
+            public int Mixer;
+            public int Fan;
+            public int Extra;
+
+            public PowerStatus()
+            {
+                Speaker      = 0;
+                Subwoofer    = 0;
+                Preamplifier = 0;
+                Mixer        = 0;
+                Fan          = 0;
+                Extra        = 0;
+            }
+
+            public string Output
+            {
+                get
+                {
+                    return string.Format("{0:D},{1:D},{2:D},{3:D},{4:D},{5:D}\r\n",
+                        Speaker, Subwoofer, Preamplifier, Mixer, Fan, Extra);
+                }
+            }
+        }
+    }
+}
